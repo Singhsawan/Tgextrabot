12b2790f39f0a9726e1a90022099d2c13366978f
Rishikesh Sharma(Rk_botz) <118276768+Rishi09090909@users.noreply.github.com>
2023-07-04 23:03:00 +0530

    Update add_movies.py

--- plugins/add_movies.py
+++ plugins/add_movies.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, Photo, Document
 from .database import collection
 from config import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -15,19 +15,19 @@ async def web_db(c: Client, m: Message):
         media = m.media
 
         # Upload image to Telegraph and get the URL
-        if isinstance(media, pyrogram.types.Photo):
+        if isinstance(media, Photo):
             image_path = await c.download_media(media)
-            telegraph_url = await c.create_telegraph_page(title='Image', content=[Image(image_path)])
-        elif isinstance(media, pyrogram.types.Document):
+            telegraph_url = await c.create_telegraph_page(title='Image', content=[telegraph.File.from_path(image_path)])
+        elif isinstance(media, Document):
             image_path = await c.download_media(media)
-            telegraph_url = await c.create_telegraph_page(title='Document', content=[Document(image_path)])
+            telegraph_url = await c.create_telegraph_page(title='Document', content=[telegraph.File.from_path(image_path)])
         else:
             telegraph_url = None
 
         id = collection.insert_one({
             "caption": message.html,
             "title": message.splitlines()[0],
-            "image_url": telegraph_url if telegraph_url else None
+            "image_url": telegraph_url.url if telegraph_url else None
         })
 
         reply_markup = InlineKeyboardMarkup([
@@ -43,4 +43,4 @@ async def web_db(c: Client, m: Message):
         await asyncio.sleep(AUTO_DELETE_TIME)
         await m.delete()
         await txt.delete()
-                      
+            
