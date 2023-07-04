a5aeefe2a7f20c50bcdce3faea703442bcf3665c
Rishikesh Sharma(Rk_botz) <118276768+Rishi09090909@users.noreply.github.com>
2023-07-04 22:56:21 +0530

    Update add_movies.py

--- plugins/add_movies.py
+++ plugins/add_movies.py
@@ -1,37 +1,46 @@
-
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from .database import collection
 from config import *
-from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import pymongo
+import telegraph
 
 # Add Movie to database
 @Client.on_message(filters.chat(ADMINS) & filters.media)
 async def web_db(c: Client, m: Message):
     if m.media:
         message = m.caption
+        media = m.media
 
-        id = collection.insert_one(
-            {"caption": message.html,
-                'title': message.splitlines()[0]}
-        )
+        # Upload image to Telegraph and get the URL
+        if isinstance(media, pyrogram.types.Photo):
+            image_path = await c.download_media(media)
+            telegraph_url = await c.create_telegraph_page(title='Image', content=[Image(image_path)])
+        elif isinstance(media, pyrogram.types.Document):
+            image_path = await c.download_media(media)
+            telegraph_url = await c.create_telegraph_page(title='Document', content=[Document(image_path)])
+        else:
+            telegraph_url = None
 
-        reply_markup = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("Delete", callback_data=f"delete#{id.inserted_id }")],
+        id = collection.insert_one({
+            "caption": message.html,
+            "title": message.splitlines()[0],
+            "image_url": telegraph_url if telegraph_url else None
+        })
+
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Delete", callback_data=f"delete#{id.inserted_id}")]
         ])
 
         txt = await m.reply('Added Successfully', reply_markup=reply_markup)
-
     else:
         txt = await m.reply('Something went wrong')
-        
+
     # Auto Delete
     if AUTO_DELETE is not False:
         await asyncio.sleep(AUTO_DELETE_TIME)
         await m.delete()
         await txt.delete()
-
-
+                      
